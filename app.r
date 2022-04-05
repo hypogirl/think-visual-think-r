0ca6776893ca6488ed2a70814b9da7ffe814a4c2
joão <74837451+slocknad@users.noreply.github.com>
2022-04-05 11:29:38 +0100

    FIXING FILE UPLOAD

--- app.r
+++ app.r
@@ -79,9 +79,6 @@ ui <- fluidPage(
             tags$link(
                 href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css", rel="stylesheet", integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3", crossorigin="anonymous"
             ),
-            tags$script(
-                src="https://code.jquery.com/jquery-3.6.0.min.js"
-            ),
             tags$link(
                 rel="stylesheet", href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css"
             ),
@@ -174,11 +171,11 @@ ui <- fluidPage(
                 )
             ),
             tags$div(class = "header", checked = NA,
-            fileInput("file1", "Choose a CSV File",
-                        multiple = FALSE,
-                        accept = c("text/csv",
-                                    "text/comma-separated-values,text/plain",
-                                    ".csv"))
+                fileInput("file1", "Choose a CSV File",
+                            multiple = FALSE,
+                            accept = c("text/csv",
+                                        "text/comma-separated-values,text/plain",
+                                        ".csv"))
             ),
             
             actionButton(
