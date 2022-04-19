8fc4faa5a4c447de7dec7f1ffa026ec7382d881c
joão <74837451+slocknad@users.noreply.github.com>
2022-04-19 10:38:04 +0100

    jquery working, html fixed

--- app.r
+++ app.r
@@ -1,3 +1,5 @@
+# adicionar install.package()
+
 library(shiny)
 library(modeldata)
 library(DAAG)
@@ -8,9 +10,6 @@ library(RCurl)
 library(httr)
 library(magick)
 library(gganimate)
-require(ggplot2)
-require(gganimate)
-require(magick)
 
 # global variables
 csvfile <- NULL
@@ -69,129 +68,78 @@ server <- function(input, output) {
 
 ui <- fluidPage(
     tags$html(
-        tags$head(
-            tags$meta(
-                charset="UTF-8"
-            ),
-            tags$meta(
-            httpequiv="X-UA-Compatible", content="IE=edge" 
-            ),
-            tags$link(
-                href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css", rel="stylesheet", integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3", crossorigin="anonymous"
-            ),
-            tags$link(
-                rel="stylesheet", href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css"
-            ),
-            tags$style(".bg-primary {
-    background-color: #1A374D!important;
-}
+        HTML('<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Think Visual :: Think R</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <nav class="navbar navbar-dark bg-primary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Think Visual :: Think R</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="offcanvas bg-secondary offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                <div class="offcanvas-header">
+                <h5 class="offcanvas-title text-light" id="offcanvasNavbarLabel">Think Visual :: Think R</h5>
+                <button type="button" class="btn-close btn-close-light text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+                <div class="offcanvas-body">
+                <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                    <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="index.html">Home</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="https://github.com/slocknad/think-visual-think-r">Github Repo <i class="bi bi-github"></i></a>
+                    </li>
+                </ul>
+                </div>
+            </div>
+        </div>
+    </nav>
+    
+    <div class="container m-0">
+        <div class="row p-3">
+            <div class="col-5" id="shinytest">
+                <h4>Import a file:</h4>
+                    <div class="rounded border border-primary text-center mt-3 p-3" id="dragdrop">
+                        <input type="file" name="dataFile" id="dataFile" onchange="importData(this.files)" style="display: none;">
+                        <label for="dataFile" style="display: block; cursor: pointer;"><i class="bi bi-upload"></i></label>
+                        <label for="dataFile" style="text-decoration: underline;cursor: pointer;">Choose a file</label> or drag it here.
+                    </div>
+            <div class="col-7" id="plot"></div>
+            </div>
+        </div>
+    </div>
 
-.bg-secondary {
-    background-color: #406882!important;
-}
 
-.border-secondary {
-    background-color: #406882;
-}
-
-.btn-close-light {
-    background-color: white;
-}
-
-.input-group {
-    width: 60%;
-}
-
-#dragdrop {
-    height: 10rem;
-    width: 25rem;
-    display: table-cell;
-    vertical-align: middle;
-    transition: 0.5s;
-}
-
-#dragdrop.highlight {
-    border-style: dashed!important;
-    border-color: #1A374D!important;
-    background-color: #B1D0E0!important;
-    transition: 0.5s;
-}")
-        ),
-        tags$body(
-            tags$nav(
-                class="navbar navbar-dark bg-primary",
-                tags$div(
-                    class="container-fluid",
-                    tags$a(
-                        class="navbar-brand", href="#",
-                        "Think Visual :: Think R"
-                    ),
-                    tags$button(
-                        class="navbar-toggler", type="button",
-                        tags$span (
-                            class="navbar-toggler-icon"
-                        )
-                    ),
-                    tags$div (
-                        class="offcanvas bg-secondary offcanvas-end", tabindex="-1", id="offcanvasNavbar",
-                        tags$div(
-                            class="offcanvas-header",
-                            tags$h5(
-                                class="offcanvas-title text-light", id="offcanvasNavbarLabel", 
-                                "Think Visual :: Think R"
-                            ),
-                            tags$button(
-                                type="button", class="btn-close btn-close-light text-reset"
-                            )
-                        ),
-                        tags$div(
-                            class="offcanvas-body",
-                            tags$ul(
-                                class="navbar-nav justify-content-end flex-grow-1 pe-3",
-                                tags$li(
-                                    class="nav-item",
-                                    tags$a(
-                                        class="nav-link active", href="index.html",
-                                        "Home"
-                                    )
-                                ),
-                                tags$li(
-                                    class="nav-item",
-                                    tags$a(
-                                        class="nav-link active", href="https://github.com/slocknad/think-visual-think-r",
-                                        "Github Repo",
-                                        tags$i(
-                                            class="bi bi-github"
-                                        )
-                                    )
-                                )
-                            )
-                        )
-                    )
-                )
-            ),
-            tags$div(class = "header", checked = NA,
-                fileInput("file1", "Choose a CSV File",
-                            multiple = FALSE,
-                            accept = c("text/csv",
-                                        "text/comma-separated-values,text/plain",
-                                        ".csv"))
-            ),
-            
-            actionButton(
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="main.js"></script>
+    <script src="jquerytest.js"></script>
+</body>
+</html>')
+    ),
+        fileInput("file1", "Choose a CSV File",
+                    multiple = FALSE,
+                    accept = c("text/csv",
+                    "text/comma-separated-values,text/plain",
+                    ".csv")
+                ),
+        actionButton(
                 inputId = "upload_button",
                 label = "Upload"),
-            
-            h4("Plot output",align="center"),
-            div(plotlyOutput("plot",width = "500px", height = "300px"), align = "center"),
-            tags$script(
-                src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js", integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p", crossorigin="anonymous"
-            )
-        )
-    )   
+        div(plotlyOutput("plot",width = "500px", height = "300px"), align = "center")
 )
 
-shinyApp(ui = htmlTemplate("www/index.html"), server)
+
+shinyApp(ui, server)
 
 
 
